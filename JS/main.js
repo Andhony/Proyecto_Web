1d1263bbaacf2f3aab3d94c7d92b401a6b074925
Andhony Hernández <andhohdz.gm@hotmail.com>
2019-06-06 05:38:00 -0600

    Modificando estructura y agredando slider

--- JS/main.js
+++ JS/main.js
@@ -4,8 +4,17 @@ $(document).ready(function(){
     $(window).on('scroll', function(){
         if ($(window).scrollTop() > altura) {
             $(".menu").addClass('menu-fixed');
+            $(".contenedor").addClass('menu-fixed-margin');
         } else {
             $(".menu").removeClass('menu-fixed');
+            $(".contenedor").removeClass('menu-fixed-margin');
         }
-    })
+    });
+
+    $(function(){
+        $(".slider-galeria article:gt(0)").hide();
+        setInterval(function(){
+            $(".slider-galeria article:first-child").fadeOut(0).next('article').fadeIn(1000)
+            .end().appendTo(".slider-galeria");},5000);
+    });
 });

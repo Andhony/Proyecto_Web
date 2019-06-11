e56a0ef5ee886ac0ce11a7981f7635d7b01c98d6
Jeymi-Aguilar <jeymibrisel@gmail.com>
2019-06-11 02:50:38 -0600

    Ocultando boton en escritorio y arreglando links

---
+++ JS/postres.js
@@ -0,0 +1,19 @@
+function mostrar(){
+    document.getElementById("paraocultar").style.display = "block";
+}
+function ocultar(){
+    document.getElementById("paraocultar").style.display = "none";
+}
+
+function mostrarg(){
+    document.getElementById("ocultargalletas").style.display = "block";
+}
+function ocultarg(){
+    document.getElementById("ocultargalletas").style.display = "none";
+}
+function mostrarc(){
+    document.getElementById("ocultarcupcakes").style.display = "block";
+}
+function ocultarc(){
+    document.getElementById("ocultarcupcakes").style.display = "none";
+}

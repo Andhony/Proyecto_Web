553a9eaefa5c33fdc9090b25bdb90fbc8d1d4b4c
Andhony Hernández <andhohdz.gm@hotmail.com>
2019-06-12 01:15:25 -0600

    Añadiendo carretilla a postres

--- JS/postres.js
+++ JS/postres.js
@@ -17,3 +17,28 @@ function mostrarc(){
 function ocultarc(){
     document.getElementById("ocultarcupcakes").style.display = "none";
 }
+
+var numCompras = 0;
+function addCarretilla(){
+    numCompras += 1;
+    let textoNum = document.getElementById('Carret');
+    textoNum.innerHTML = numCompras;
+}
+function rmvCarretilla(){
+    numCompras -= 1;
+    let textoNum = document.getElementById('Carret');
+    textoNum.innerHTML = numCompras;
+}
+
+function hideBoton(id){
+    let idmostrar = id + 1;
+    console.log("Entro!");
+    document.getElementById(id).style.display = "none";
+    document.getElementById(idmostrar).style.display = "block";
+}
+function mostrarBoton(id){
+    let idmostrar = id - 1;
+    console.log("Entro!");
+    document.getElementById(id).style.display = "none";
+    document.getElementById(idmostrar).style.display = "block";
+}
